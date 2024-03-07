d11a6b9ee994ef5afa7f3f197515fd9807f33679
Noshin03 <69012995+Noshin03@users.noreply.github.com>
2024-03-07 14:05:30 -0500

    set up front page
    to get started, added some content to front page, e.g buttons, header, background image

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h1> Start of SOSO Front-npEnd </h1>
+    <header className="App-header">
+    <h1 style={{ color: 'white' }}>SOSO Satellite Optimizer</h1>
+    <div className="button-container">
+      <button className="button">Add Satellite</button>
+      <button className="button">Add Ground Station</button>
+      <button className="button">Add Task</button>
+      <button className="button">Perform Scheduling</button>
     </div>
+  </header>
   );
 }
 
